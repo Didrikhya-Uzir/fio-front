9ed1be2817e481308c8b8f9562f5271e4cc64bfe
rockysaikia730 <rockysaikia730@gmail.com>
2021-06-21 23:31:11 +0530

    "shop-home added"

---
+++ src/views/ShoppingHome.vue
@@ -0,0 +1,52 @@
+<template>
+ 
+    <carousel />
+
+    <section id="electronics">
+        <h1>Electronics</h1>
+        <horizontalscroll/>
+    </section>
+  
+     <section id="Clothing">
+        <h1>Clothing</h1>
+        <horizontalscroll/>
+    </section>
+
+     <section id="Influencers">
+        <h1>Influencers</h1>
+        <horizontalscroll/>
+    </section>
+
+     <section id="Grocery">
+        <h1>Grocery</h1>
+        <horizontalscroll/>
+    </section>
+
+    
+</template>
+
+<script>
+import fionavbar  from '../components/fionavbar.vue'
+import carousel from '../components/carousel.vue'
+import horizontalscroll from '../components/horizontalscroll.vue'
+import footerfio from '../components/footerfio.vue'
+
+export default {
+    name: 'ShoppingHome',
+    components: {
+        carousel,
+        fionavbar,
+        footerfio,
+        horizontalscroll
+    }
+}
+
+</script> 
+
+
+<style scoped>
+
+</style>
+
+
+       
